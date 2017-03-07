a975f2106dc45a5672c0509fbe8920f6f7ef8510
daveho8888 <daveho8888@gmail.com>
2017-03-07 14:08:07 -0800

    huge ui updates

--- ChewSnap/app/src/main/java/io/zetaphase/chewsnap/MealListFragment.java
+++ ChewSnap/app/src/main/java/io/zetaphase/chewsnap/MealListFragment.java
@@ -6,7 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Dave Ho on 3/6/2017.
@@ -14,15 +18,22 @@ import android.widget.ListView;
 
 public class MealListFragment extends Fragment {
 
-    ListView mealListView;
+    private ListView mealListView;
+    private ArrayAdapter<String> listAdapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.meal_list_fragment, container , false);
-        mealListView = (ListView) getView().findViewById(R.id.mealList);
+        mealListView = (ListView) view.findViewById(R.id.mealList);
+        String[] meals = new String[] {"apple", "steak"};
+        ArrayList<String> mealList = new ArrayList<>();
+        mealList.addAll(Arrays.asList(meals));
+        listAdapter = new ArrayAdapter<String>(getActivity(), R.layout.meal_row, mealList);
+        mealListView.setAdapter(listAdapter);
         return view;
     }
+
 
 
 }
