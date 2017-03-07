ce446035aa63b0003ba4b6211cfd2fab0471bb2e
daveho8888 <daveho8888@gmail.com>
2017-03-07 11:42:13 -0800

    update ui of meal_list_fragment

--- ChewSnap/app/src/main/java/io/zetaphase/chewsnap/MealListFragment.java
+++ ChewSnap/app/src/main/java/io/zetaphase/chewsnap/MealListFragment.java
@@ -6,15 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * Created by Dave Ho on 3/6/2017.
  */
 
 public class MealListFragment extends Fragment {
+
+    ListView mealListView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.meal_list_fragment, container, false);
+        View view = inflater.inflate(R.layout.meal_list_fragment, container , false);
+        mealListView = (ListView) getView().findViewById(R.id.mealList);
+        return view;
     }
+
+
 }
