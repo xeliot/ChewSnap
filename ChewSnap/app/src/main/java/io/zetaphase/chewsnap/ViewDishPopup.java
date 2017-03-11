ce55b437bad60dd9fe24355a063c5279beba98f7
daveho8888 <daveho8888@gmail.com>
2017-03-10 17:04:02 -0800

    adjust manifest file to accomodate new activity

--- ChewSnap/app/src/main/java/io/zetaphase/chewsnap/ViewDishPopup.java
+++ ChewSnap/app/src/main/java/io/zetaphase/chewsnap/ViewDishPopup.java
@@ -1,8 +1,27 @@
 package io.zetaphase.chewsnap;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
 /**
  * Created by Dave Ho on 3/10/2017.
  */
 
-public class ViewDishPopup {
+public class ViewDishPopup extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.view_dish_popup);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width*0.8), (int) (height*0.8));
+
+    }
 }
