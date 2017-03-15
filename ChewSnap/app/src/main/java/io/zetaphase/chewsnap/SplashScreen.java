b4322c8f4e5f5ddcb76cc044ed8b443032a61277
daveho8888 <daveho8888@gmail.com>
2017-03-14 19:06:17 -0700

    add splash screen

---
+++ ChewSnap/app/src/main/java/io/zetaphase/chewsnap/SplashScreen.java
@@ -0,0 +1,31 @@
+package io.zetaphase.chewsnap;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by Dave Ho on 3/14/2017.
+ */
+
+public class SplashScreen extends Activity{
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
