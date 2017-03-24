381709aae1758d229f41fdcb5698437ce7a2df2c
daveho8888 <daveho8888@gmail.com>
2017-03-23 19:35:53 -0700

    serveraddress home localhost

--- ChewSnap/app/src/main/java/io/zetaphase/chewsnap/LoginActivity.java
+++ ChewSnap/app/src/main/java/io/zetaphase/chewsnap/LoginActivity.java
@@ -35,8 +35,8 @@ public class LoginActivity extends Activity {
     TextView _signupLink;
     String response;
 
-    //private String serverAddress = "192.168.1.65";
-    private String serverAddress = "10.78.43.147";
+    private String serverAddress = "192.168.1.65";
+    //private String serverAddress = "10.78.43.147";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
